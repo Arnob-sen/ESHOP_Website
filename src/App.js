6c2590b55bb48aa3fe96282d118da294a9397c24
Arnob-sen <arnobsen16.2@gmail.com>
2023-08-29 01:47:51 +0600

    give_multiple_component_in_one_route

--- src/App.js
+++ src/App.js
@@ -1,14 +1,44 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 import Header from './component/Header';
 import Home from "./component/Home";
+import { BrowserRouter ,Route,Routes } from "react-router-dom";
+import Checkout from "./component/Checkout";
 
 function App() {
   return (
-    <div className="App">
-        <Header/>
-        <Home />
-    </div>
+    <BrowserRouter>
+  <div className="App">
+  
+ 
+  <Routes>
+    <Route path="/checkout"
+    element={
+      <Fragment><Header/><Checkout/></Fragment>}/>
+
+    
+  <Route  path='/'
+  element={
+    <Fragment>
+      <Header/>
+      <Home />
+    </Fragment>}/>
+  
+   
+    
+    
+  
+
+    
+    
+  
+  
+  
+  </Routes>
+
+  </div>
+  </BrowserRouter>
+
   );
 }
 
